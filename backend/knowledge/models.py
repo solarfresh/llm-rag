a9f7b0d255993febeb6ddade67c288baf16322dd
Scott Huang <shangyuhuang@gmail.com>
2024-04-28 17:28:03 +0800

    feature: prepare a document template of knowledge sets

--- backend/knowledge/models.py
+++ backend/knowledge/models.py
@@ -1,8 +1,24 @@
 import uuid
+from typing import OrderedDict
 
 from django.db import models
+from django_opensearch_dsl import Document
+from django_opensearch_dsl import fields as documents
+from django_opensearch_dsl.registries import registry
 from django.utils import timezone as datetime
 from django.utils.translation import gettext_lazy as _
+from opensearchpy.helpers import field as documents_helpers
+
+
+class VectorField(
+    documents.DODField,
+    documents_helpers.Field
+):
+    name = "knn_vector"
+
+    def __init__(self, attr=None, **kwargs) -> None:
+        kwargs["multi"] = True
+        super(VectorField, self).__init__(attr=attr, **kwargs)
 
 
 class KnowledgeInfoModel(models.Model):
@@ -19,3 +35,48 @@ class KnowledgeInfoModel(models.Model):
     description = models.TextField(null=True)
     create_at = models.DateTimeField(default=datetime.now)
     modified_at = models.DateTimeField(blank=True, null=True)
+
+    def save(self, **kwargs):
+        super().save(**kwargs)
+
+class KnowledgeSetDocument:
+
+    def __new__(cls, index_name):
+        return cls.generate_document_class(index_name)
+
+    @classmethod
+    def generate_document_class(self, index_name):
+        class KnowledgeSetModel(models.Model):
+            document_id = models.UUIDField(
+                primary_key=True,
+                default=uuid.uuid4,
+                editable=False
+            )
+
+            class Meta:
+                managed = False
+
+
+        @registry.register_document
+        class KnowledgeSetDocumentTemplate(Document):
+            text = documents.TextField()
+            embedding = VectorField(
+                dimension = 796,
+                method = {
+                    "name": "hnsw",
+                    "space_type": "l2",
+                    "engine": "lucene",
+                    "parameters": {
+                        "ef_construction": 128,
+                        "m": 24
+                    }
+                }
+            )
+
+            class Django:
+                model = KnowledgeSetModel
+
+            class Index:
+                name = index_name
+
+        return KnowledgeSetDocumentTemplate
--- backend/knowledge/serializers.py
+++ backend/knowledge/serializers.py
@@ -1,5 +1,3 @@
-import uuid
-from datetime import datetime
 from rest_framework import serializers
 
 
