4263f40226db59b58806da50c4401b3a41d904b9
Scott Huang <shangyuhuang@gmail.com>
2024-04-30 21:40:21 +0800

    feature: define an uri parsing webpages

--- backend/knowledge/views.py
+++ backend/knowledge/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 from . import models, serializers
 
@@ -10,3 +11,16 @@ class KnowledgeInfoView(
     queryset = models.KnowledgeInfoModel.objects.all()
     model = models.KnowledgeInfoModel
     serializer_class = serializers.KnowledgeInfoSerializers
+
+
+class KnowdegeSetHTMLLoaderView(
+    generics.CreateAPIView,
+    generics.GenericAPIView
+):
+
+    def post(self, request, *args, **kwargs):
+        urls = request.data.get('url', None)
+        if urls is None:
+            return Response({
+                "message": "urls can not be empty."
+            }, status=status.HTTP_400_BAD_REQUEST)
