ab99185c816154cb6046ef8ddfc5bc60404e05ca
Scott Huang <shangyuhuang@gmail.com>
2024-04-30 23:24:02 +0800

    feature: parse html and split texts

--- backend/knowledge/views.py
+++ backend/knowledge/views.py
@@ -1,3 +1,6 @@
+from langchain_community.document_loaders.url import UnstructuredURLLoader
+from langchain_community.vectorstores.opensearch_vector_search import OpenSearchVectorSearch
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from rest_framework import generics, status
 from rest_framework.response import Response
 
@@ -19,8 +22,24 @@ class KnowdegeSetHTMLLoaderView(
 ):
 
     def post(self, request, *args, **kwargs):
-        urls = request.data.get('url', None)
+        urls = request.data.get('urls', None)
         if urls is None:
             return Response({
                 "message": "urls can not be empty."
             }, status=status.HTTP_400_BAD_REQUEST)
+
+        loader = UnstructuredURLLoader(urls=urls)
+        data = loader.load()
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=0
+        )
+        documents = splitter.split_documents(data)
+
+        return Response([
+            {
+                "page_content": doc.page_content,
+                "metadata": doc.metadata
+            }
+            for doc in documents
+        ], status=status.HTTP_200_OK)
